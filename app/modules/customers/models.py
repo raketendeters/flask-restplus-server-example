e2774b2888124ae4c1879a0def66b3bb6856916b
Philipp Deters <deters@amicaldo.de>
2019-07-24 16:21:17 +0200

    changed sqla customer model

--- app/modules/customers/models.py
+++ app/modules/customers/models.py
@@ -13,9 +13,47 @@ class Customer(db.Model, Timestamp):
     """
     Customers database model.
     """
-
-    id = db.Column(db.Integer, primary_key=True) # pylint: disable=invalid-name
-    title = db.Column(db.String(length=50), nullable=False)
+    # pylint: disable=invalid-name
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=invalid-name
+    title = db.Column(db.String(length=250), nullable=False)
+    customer_number = db.Column(db.String(length=250), unique=True)
+    customer_id = db.Column(db.String(length=250), unique=True)
+    days_for_payment = db.Column(db.Integer)
+    payment_type = db.Column(db.String(length=200), nullable=True)
+    bank_name = db.Column(db.String(length=200), nullable=True)
+    bank_account_number = db.Column(db.String(length=200), nullable=True)
+    bank_code = db.Column(db.String(length=200), nullable=True)
+    bank_account_owner = db.Column(db.String(length=200), nullable=True)
+    bank_iban = db.Column(db.String(length=200), nullable=True)
+    bank_bic = db.Column(db.String(length=200), nullable=True)
+    bank_account_mandate_reference = db.Column(db.String(length=200), nullable=True)
+    show_payment_notice = db.Column(db.Boolean, default=1)
+    account_receivable = db.Column(db.String(length=200), nullable=True)
+    customer_type = db.Column(db.String(length=200), nullable=True)
+    top = db.Column(db.String(length=200), nullable=True)
+    newsletter_optin = db.Column(db.Boolean, default=1)
+    organization = db.Column(db.String(length=200), nullable=True)
+    position = db.Column(db.String(length=200), nullable=True)
+    academic_degree = db.Column(db.String(length=200), nullable=True)
+    salutation = db.Column(db.String(length=200), nullable=True)
+    first_name = db.Column(db.String(length=200), nullable=True)
+    last_name = db.Column(db.String(length=200), nullable=True)
+    address = db.Column(db.String(length=200), nullable=True)
+    address_2 = db.Column(db.String(length=200), nullable=True)
+    zipcode = db.Column(db.String(length=200), nullable=True)
+    city = db.Column(db.String(length=200), nullable=True)
+    country_code = db.Column(db.String(length=200), nullable=True)
+    secondary_address = db.Column(db.String(length=200), nullable=True)
+    phone = db.Column(db.String(length=200), nullable=True)
+    phone_2 = db.Column(db.String(length=200), nullable=True)
+    fax = db.Column(db.String(length=200), nullable=True)
+    mobile = db.Column(db.String(length=200), nullable=True)
+    email = db.Column(db.String(length=200), nullable=True)
+    website = db.Column(db.String(length=200), nullable=True)
+    vat_id = db.Column(db.String(length=200), nullable=True)
+    currency_code = db.Column(db.String(length=200), nullable=True)
+    tags = db.Column(db.String(length=200), nullable=True)
+    created_on_fb = db.Column(db.String(length=200), nullable=True)
 
     def __repr__(self):
         return (
@@ -29,7 +67,7 @@ class Customer(db.Model, Timestamp):
         )
 
     @db.validates('title')
-    def validate_title(self, key, title): # pylint: disable=unused-argument,no-self-use
+    def validate_title(self, key, title):  # pylint: disable=unused-argument,no-self-use
         if len(title) < 3:
             raise ValueError("Title has to be at least 3 characters long.")
         return title
